f7d2c46734dbc6fe72317c8bba56631b62a53673
SoN41 <ranasujal04@gmail.com>
2023-01-21 15:11:41 +0530

    SecondCommit

--- Core/index.js
+++ Core/index.js
@@ -1,2 +1,14 @@
-const express = require('express')
-const Blockchain = require('./blockchain')
+const express = require('express');
+const Blockchain = require('./blockchain');
+
+const app = express();
+const blockchain = new Blockchain();
+
+app.get("/api/blocks",(req,res)=>{
+    res.json(blockchain.chain)
+})
+
+const PORT = 8080;
+app.listen(PORT,()=>{
+    console.log(`listening to PORT : ${PORT}`);
+});
